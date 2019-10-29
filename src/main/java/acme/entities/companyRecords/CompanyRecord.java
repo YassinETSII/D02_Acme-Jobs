0d6f5432a2bf122e01e8a9036c6cdd7e63450bb1
YassinETSII <mohlalela@us.es>
2019-10-29 18:26:39 +0100

    Adding annotations and <<entity>> header on each entity on the UML.

--- src/main/java/acme/entities/companyRecords/CompanyRecord.java
+++ src/main/java/acme/entities/companyRecords/CompanyRecord.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.URL;
@@ -37,6 +38,7 @@ public class CompanyRecord extends DomainEntity {
 	@NotBlank
 	private String				description;
 
+	@NotBlank
 	@URL
 	private String				web;
 
@@ -48,6 +50,7 @@ public class CompanyRecord extends DomainEntity {
 	@Email
 	private String				email;
 
+	@NotNull
 	private Boolean				incorporated;
 
 	@Min(0)
